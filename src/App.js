e4f48fef2ef33f56d2e68bd83e74405ba86414ba
Vladimir Pavlychev <vladimir.pavlychev@osarts.ru>
2019-01-11 17:05:18 +0300

    Fix: :recycle: Move project files from ../mya-app to /

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Привет всем интенсивщикам на React-App 3Days!</h1>
+      </div>
+    );
+
+    //return (
+    // <img src="/lessons/day1/my-app/src/img/slide-1.png">
+    //);
+  }
+}
+
+export default App;
